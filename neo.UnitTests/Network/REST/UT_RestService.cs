f227aa37a71a4becd5701e6dbc97f7109492da1c
name-unavailable <name_unavailable@163.com>
2019-09-30 14:42:09 +0800

    restservice fix

---
+++ neo.UnitTests/Network/REST/UT_RestService.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Neo.UnitTests.Network.REST
+{
+    [TestClass]
+    public class UT_RestService
+    {
+
+    }
+}
--- neo/Network/Restful/RestService.cs
+++ neo/Network/Restful/RestService.cs
@@ -9,38 +9,35 @@ namespace Neo.Network.Restful
 {
     public class RestService : QueryServer, IRestService
     {
-        new ActionResult<JObject> GetBestBlockHash() => base.GetBestBlockHash();
+        public new ActionResult<JObject> GetBestBlockHash() => base.GetBestBlockHash();
 
-        new ActionResult<JObject> GetBlock(JObject key, int verbose)
-        {
+        public new ActionResult<JObject> GetBlock(JObject key, bool verbose) => base.GetBlock(key, verbose);
 
-        }
+        public new ActionResult<JObject> GetBlockCount() => base.GetBlockCount();
 
-        new ActionResult<JObject> GetBlockCount() => base.GetBlockCount();
+        public new ActionResult<JObject> GetBlockHash(uint height) => base.GetBlockHash(height);
 
-        new ActionResult<JObject> GetBlockHash(uint height) => base.GetBlockHash(height);
+        public new ActionResult<JObject> GetBlockHeader(JObject key, bool verbose) => base.GetBlockHeader(key, verbose);
 
-        new ActionResult<JObject> GetBlockHeader(JObject key, bool verbose) => base.GetBlockHeader(key, verbose);
+        public new ActionResult<JObject> GetBlockSysFee(uint height) => base.GetBlockSysFee(height);
 
-        new ActionResult<JObject> GetBlockSysFee(uint height) => base.GetBlockSysFee(height);
+        public new ActionResult<JObject> GetContractState(UInt160 script_hash) => base.GetContractState(script_hash);
 
-        new ActionResult<JObject> GetContractState(UInt160 script_hash) => base.GetContractState(script_hash);
+        public new ActionResult<JObject> GetRawMemPool(bool shouldGetUnverified) => base.GetRawMemPool(shouldGetUnverified);
 
-        new ActionResult<JObject> GetRawMemPool(bool shouldGetUnverified) => base.GetRawMemPool(shouldGetUnverified);
+        public new ActionResult<JObject> GetRawTransaction(UInt256 hash, bool verbose) => base.GetRawTransaction(hash, verbose);
 
-        new ActionResult<JObject> GetRawTransaction(UInt256 hash, bool verbose) => base.GetRawTransaction(hash, verbose);
+        public new ActionResult<JObject> GetStorage(UInt160 script_hash, byte[] key) => base.GetStorage(script_hash, key);
 
-        new ActionResult<JObject> GetStorage(UInt160 script_hash, byte[] key) => base.GetStorage(script_hash, key);
+        public new ActionResult<JObject> GetTransactionHeight(UInt256 hash) => base.GetTransactionHeight(hash);
 
-        new ActionResult<JObject> GetTransactionHeight(UInt256 hash) => base.GetTransactionHeight(hash);
+        public new ActionResult<JObject> GetValidators() => base.GetValidators();
 
-        new ActionResult<JObject> GetValidators() => base.GetValidators();
+        public new ActionResult<JObject> GetVersion() => base.GetVersion();
 
-        new ActionResult<JObject> GetVersion() => base.GetVersion();
+        public new ActionResult<JObject> InvokeFunction(UInt160 script_hash, string operation, ContractParameter[] args) => InvokeFunction(script_hash, operation, args);
 
-        new ActionResult<JObject> InvokeFunction(UInt160 script_hash, string operation, ContractParameter[] args) => InvokeFunction(script_hash, operation, args);
-
-        ActionResult<JObject> InvokeScript(byte[] script, UInt160[] scriptHashesForVerifying)
+        public ActionResult<JObject> InvokeScript(byte[] script, UInt160[] scriptHashesForVerifying)
         {
             CheckWitnessHashes checkWitnessHashes = null;
             if(scriptHashesForVerifying != null)
@@ -50,13 +47,13 @@ namespace Neo.Network.Restful
             return base.InvokeScript(script, checkWitnessHashes);
         }
 
-        new ActionResult<JObject> ListPlugins() => base.ListPlugins();
+        public new ActionResult<JObject> ListPlugins() => base.ListPlugins();
 
-        new ActionResult<JObject> SendRawTransaction(Transaction tx) => base.SendRawTransaction(tx);
+        public new ActionResult<JObject> SendRawTransaction(Transaction tx) => base.SendRawTransaction(tx);
 
-        new ActionResult<JObject> SubmitBlock(Block block) => base.SubmitBlock(block);
+        public new ActionResult<JObject> SubmitBlock(Block block) => base.SubmitBlock(block);
 
-        new ActionResult<JObject> ValidateAddress(string address) => base.ValidateAddress(address);
+        public new ActionResult<JObject> ValidateAddress(string address) => base.ValidateAddress(address);
 
     }
 }
