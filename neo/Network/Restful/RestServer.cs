8922e2cdc085c15b5161ae41e179cb3d74d75385
name-unavailable <name_unavailable@163.com>
2019-09-30 15:28:17 +0800

    add swagger

--- neo/Network/Restful/RestServer.cs
+++ neo/Network/Restful/RestServer.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Neo.Network.Restful
 {
@@ -17,6 +18,15 @@ namespace Neo.Network.Restful
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new Info
+                {
+                    Title = "Neo Rest API",
+                    Version = "v1"
+                });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -32,6 +42,15 @@ namespace Neo.Network.Restful
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Neo Rest API");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseMvc();
         }
     }
